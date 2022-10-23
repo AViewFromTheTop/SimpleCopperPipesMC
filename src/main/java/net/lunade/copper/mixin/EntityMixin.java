6e96ac434b43b2cea358e3be5ca72b12aa3c5227
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-10-23 17:32:18 -0400

    Update EntityMixin.java

--- src/main/java/net/lunade/copper/mixin/EntityMixin.java
+++ src/main/java/net/lunade/copper/mixin/EntityMixin.java
@@ -5,31 +5,32 @@ import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
+    /*
+    public boolean hadWaterPipeNearby;
+
+    @Inject(at = @At("HEAD"), method = "updateInWaterStateAndDoWaterCurrentPushing")
+    public void updateInWaterStateAndDoWaterCurrentPushing(CallbackInfo info) {
+        Entity entity = Entity.class.cast(this);
+        this.hadWaterPipeNearby = CopperPipe.isWaterPipeNearby(entity.level, entity.blockPosition(), 2);
+    }
 
     @Inject(at = @At("TAIL"), method = "isInWaterRainOrBubble", cancellable = true)
     public void isInWaterRainOrBubble(CallbackInfoReturnable<Boolean> info) {
-        /*
-        Entity entity = Entity.class.cast(this);
-        if (CopperPipe.isWaterPipeNearby(entity.level, entity.blockPosition(), 2)) {
+        if (this.hadWaterPipeNearby) {
             info.setReturnValue(true);
-            info.cancel();
         }
-        
-         */
     }
 
     @Inject(at = @At("TAIL"), method = "isInWaterOrBubble", cancellable = true)
     public void isInWaterOrBubble(CallbackInfoReturnable<Boolean> info) {
-        /*
-        Entity entity = Entity.class.cast(this);
-        if (CopperPipe.isWaterPipeNearby(entity.level, entity.blockPosition(), 2)) {
+        if (this.hadWaterPipeNearby) {
             info.setReturnValue(true);
-            info.cancel();
         }
-         */
     }
+     */
 }

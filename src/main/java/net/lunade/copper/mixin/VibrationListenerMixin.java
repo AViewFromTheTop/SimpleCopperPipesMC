1e1a3a54a42475684eb4532bfb8dacaab8ed39c8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-10-23 17:15:37 -0400

    Why Won't It Work

--- src/main/java/net/lunade/copper/mixin/VibrationListenerMixin.java
+++ src/main/java/net/lunade/copper/mixin/VibrationListenerMixin.java
@@ -23,15 +23,12 @@ public class VibrationListenerMixin {
     public void handleGameEvent(ServerLevel serverLevel, GameEvent gameEvent, GameEvent.Context context, Vec3 vec3, CallbackInfoReturnable<Boolean> infoReturnable) {
         if (infoReturnable.getReturnValue()) {
             BlockPos checkPos = new BlockPos(vec3);
-            BlockState state = serverLevel.getBlockState(checkPos);
-            if (state.getBlock() instanceof CopperPipe) {
-                BlockEntity blockEntity = serverLevel.getBlockEntity(checkPos);
-                if (blockEntity instanceof CopperPipeEntity pipeEntity) {
-                    if (pipeEntity.inputGameEventPos != null && pipeEntity.gameEventNbtVec3 != null && pipeEntity.noteBlockCooldown <= 0) {
-                        serverLevel.sendParticles(new VibrationParticleOption(new BlockPositionSource(checkPos), 5), pipeEntity.gameEventNbtVec3.x(), pipeEntity.gameEventNbtVec3.y(), pipeEntity.gameEventNbtVec3.z(), 1, 0.0D, 0.0D, 0.0D, 0.0D);
-                        pipeEntity.inputGameEventPos = null;
-                        pipeEntity.gameEventNbtVec3 = null;
-                    }
+            BlockEntity blockEntity = serverLevel.getBlockEntity(checkPos);
+            if (blockEntity instanceof CopperPipeEntity pipeEntity) {
+                if (pipeEntity.inputGameEventPos != null && pipeEntity.gameEventNbtVec3 != null && pipeEntity.noteBlockCooldown <= 0) {
+                    serverLevel.sendParticles(new VibrationParticleOption(new BlockPositionSource(checkPos), 5), pipeEntity.gameEventNbtVec3.x(), pipeEntity.gameEventNbtVec3.y(), pipeEntity.gameEventNbtVec3.z(), 1, 0.0D, 0.0D, 0.0D, 0.0D);
+                    pipeEntity.inputGameEventPos = null;
+                    pipeEntity.gameEventNbtVec3 = null;
                 }
             }
         }

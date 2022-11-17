8a49e66180bcbaa7fc30cda3a02da6bd619e7323
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-17 16:42:34 -0500

    Not Cutouts

--- src/main/java/net/lunade/copper/CopperPipeClient.java
+++ src/main/java/net/lunade/copper/CopperPipeClient.java
@@ -36,94 +36,6 @@ public class CopperPipeClient implements ClientModInitializer {
         ParticleFactoryRegistry.getInstance().register(CopperPipeMain.ORANGE_INK, PipeInkParticle.OrangeFactory::new);
         ParticleFactoryRegistry.getInstance().register(CopperPipeMain.WHITE_INK, PipeInkParticle.WhiteFactory::new);
 
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.COPPER_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.EXPOSED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WEATHERED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.OXIDIZED_PIPE, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WAXED_COPPER_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WAXED_EXPOSED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WAXED_WEATHERED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WAXED_OXIDIZED_PIPE, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.BLACK_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.RED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GREEN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.BROWN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.BLUE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.PURPLE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.CYAN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.LIGHT_GRAY_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GRAY_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.PINK_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.LIME_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.YELLOW_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.LIGHT_BLUE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.MAGENTA_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.ORANGE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.WHITE_PIPE, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_BLACK_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_RED_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_GREEN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_BROWN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_BLUE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_PURPLE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_CYAN_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_LIGHT_GRAY_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_GRAY_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_PINK_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_LIME_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_YELLOW_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_LIGHT_BLUE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_MAGENTA_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_ORANGE_PIPE, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperPipe.GLOWING_WHITE_PIPE, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.COPPER_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.EXPOSED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WEATHERED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.OXIDIZED_FITTING, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WAXED_COPPER_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WAXED_EXPOSED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WAXED_WEATHERED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WAXED_OXIDIZED_FITTING, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.BLACK_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.RED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GREEN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.BROWN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.BLUE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.PURPLE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.CYAN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.LIGHT_GRAY_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GRAY_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.PINK_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.LIME_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.YELLOW_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.LIGHT_BLUE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.MAGENTA_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.ORANGE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.WHITE_FITTING, RenderType.cutout());
-
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_BLACK_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_RED_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_GREEN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_BROWN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_BLUE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_PURPLE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_CYAN_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_LIGHT_GRAY_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_GRAY_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_PINK_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_LIME_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_YELLOW_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_LIGHT_BLUE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_MAGENTA_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_ORANGE_FITTING, RenderType.cutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(CopperFitting.GLOWING_WHITE_FITTING, RenderType.cutout());
-
         ClientPlayNetworking.registerGlobalReceiver(NOTE_PACKET, (client, handler, buf, responseSender) -> {
             BlockPos pos = buf.readBlockPos();
             int k = buf.readInt();
@@ -131,9 +43,9 @@ public class CopperPipeClient implements ClientModInitializer {
             Direction direction = getDirection(i);
             client.execute(() -> {
                 assert client.level != null;
-                double x = direction.getStepX()*0.6;
-                double y = direction.getStepY()*0.6;
-                double z = direction.getStepZ()*0.6;
+                double x = direction.getStepX() * 0.6;
+                double y = direction.getStepY() * 0.6;
+                double z = direction.getStepZ() * 0.6;
                 client.level.addParticle(ParticleTypes.NOTE, (double)pos.getX() + 0.5D + x, (double)pos.getY() + 0.5D + y, (double)pos.getZ() + 0.5D + z, (double)k / 24.0D, 0.0D, 0.0D);
             });
         });

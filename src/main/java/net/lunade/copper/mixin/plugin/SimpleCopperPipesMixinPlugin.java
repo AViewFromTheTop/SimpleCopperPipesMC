c3cf3de64515e7eb9827ae127d8b578df8b4761c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-10-07 22:46:42 -0400

    2.1.1

--- src/main/java/net/lunade/copper/mixin/plugin/SimpleCopperPipesMixinPlugin.java
+++ src/main/java/net/lunade/copper/mixin/plugin/SimpleCopperPipesMixinPlugin.java
@@ -2,14 +2,12 @@ package net.lunade.copper.mixin.plugin;
 
 import java.util.List;
 import java.util.Set;
-import net.fabricmc.loader.api.FabricLoader;
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 public class SimpleCopperPipesMixinPlugin implements IMixinConfigPlugin {
-	private final boolean hasTheCopperierAge = FabricLoader.getInstance().isModLoaded("thecopperierage");
 
 	@Override
 	public void onLoad(String mixinPackage) {
@@ -22,7 +20,6 @@ public class SimpleCopperPipesMixinPlugin implements IMixinConfigPlugin {
 
 	@Override
 	public boolean shouldApplyMixin(@NotNull String targetClassName, @NotNull String mixinClassName) {
-		if (targetClassName.contains("thecopperierage.")) return this.hasTheCopperierAge;
 		return true;
 	}
 
